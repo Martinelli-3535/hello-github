fe90658e6ee82bcd80d0e0ab75a3342a785f9208
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2022-05-08 16:02:25 +0900

    2022.05.08 깃허브 커밋 예시

---
+++ toString.java
@@ -0,0 +1,15 @@
+
+public class toString {
+	int x,y;
+	public toString(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static void main(String[] args) {
+		toString t = new toString(2,3);
+		System.out.println(t);
+
+	}
+
+}
