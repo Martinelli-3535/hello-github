cc8e56ff433a8906396db9abf869b0278aaec831
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2022-09-27 10:15:46 +0900

    2022 09 27
    그동안 정리 안한것들 / 대부분 웹

---
+++ Chrome/script.js
@@ -0,0 +1,10 @@
+const player = {
+    name : "nico",
+    sayHello : function(someone) {
+        console.log("hello my name is " + someone);
+
+    },
+    age: 25
+};
+
+player.sayHello("jungwoo");
