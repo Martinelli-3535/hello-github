5307420a893c9fb76c28f7360e591b6877c7d16c
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2022-10-07 00:46:18 +0900

    2022 10 07
    리액트 시작 / 프로젝트 생성

--- Chrome/script.js
+++ Chrome/script.js
@@ -1,10 +1,5 @@
 const player = {
-    name : "nico",
-    sayHello : function(someone) {
-        console.log("hello my name is " + someone);
-
-    },
-    age: 25
+    
 };
 
 player.sayHello("jungwoo");
